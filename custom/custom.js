4b5f4fb44084eea28a0a057a391cd99c3fcfc94d
Min RK <benjaminrk@gmail.com>
2016-05-02 13:18:58 +0200

    redo custom.js
    move things to extensions

--- custom/custom.js
+++ custom/custom.js
@@ -1,33 +1,35 @@
-console.log('jupyter!');
-// autosave every 30 seconds;
 require(['notebook/js/cell', 'codemirror/keymap/sublime'], function (cell) {
-    cell.Cell.options_default.cm_config.keyMap = 'sublime';
+  cell.Cell.options_default.cm_config.keyMap = 'sublime';
 });
 
-require(['base/js/namespace', 'base/js/events', 'base/js/utils'], function (IPython, events, utils) {
-$([IPython.events]).on("notebook_loaded.Notebook", function () {
-    console.log("load");
-    IPython.notebook.default_cell_type = 'above';
-    IPython.notebook.minimum_autosave_interval = 30000;
-// });
-//
-// // when doing presentations, I hode the header and toolbar
-// $([IPython.events]).on("app_initialized.NotebookApp", function () {
-    // console.log("init");
-    // load the gist and toc extensions
-    utils.load_extensions("gist", "toc");
+// autosave every 30 seconds;
+require(['base/js/namespace', 'base/js/events', 'base/js/utils'], function (Jupyter, events, utils) {
+  console.log("requiring");
+  function onload () {
+    Jupyter.notebook.default_cell_type = 'above';
+    Jupyter.notebook.minimum_autosave_interval = 30000;
+    //
+    // Jupyter.keyboard_manager.command_shortcuts.add_shortcut(
+    //   '.', 'jupyter-notebook:toggle-cell-marked');
     
     // I have special CSS that only applies to the notebook
     $('body').addClass('notebook-body');
-    
-    // hide cell toolbar and cell type select, I never use it
+  
     $('#cell_type').hide();
-    $('#ctb_select').hide().prev().hide();
-    // $('div#header').hide();
-    // $('div#maintoolbar').hide();
-    IPython.CodeCell.options_default.cm_config.autoCloseBrackets = false;
+    // $('#ctb_select').hide().prev().hide();
+    // hide celltoobar button
+    $('button[title*=celltoolbar]').parent().hide();
+    // hide cell toolbar and cell type select, I never use it
+    // when doing presentations, I hide the header and toolbar
+    // $("#toggle_header").click();
+    // $("#toggle_toolbar").click();
 
     // autoscroll is my greatest shame:
-    IPython.OutputArea.auto_scroll_threshold = 0;
-});
+    Jupyter.OutputArea.auto_scroll_threshold = 0;
+};
+  if (Jupyter.notebook) {
+    onload();
+  } else {
+    events.on("notebook_loaded.Notebook", onload);
+  }
 });
